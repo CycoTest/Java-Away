52e614eb1d2dad801ee90e9e84b40a8d55c37831
Administrator <Administrator@User-2022HKBMAH>
2022-12-06 11:06:45 +0900

    2022/11/03 Java Class

---
+++ 2022_11_03/src/ForSumEx01.java
@@ -0,0 +1,11 @@
+public class ForSumEx01 {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=1; i<=10; i++) {
+			sum += i; // Sum = sum + i 의 줄임말
+		}
+		
+		System.out.println("sum = " + sum);
+	}
+}
