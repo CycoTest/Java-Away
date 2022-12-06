d6e6623f652bb8dca3e75ce617994976218b7ba3
Administrator <Administrator@User-2022HKBMAH>
2022-12-06 11:03:52 +0900

    2022/11/01	Java Class

---
+++ 2022_11_01/Concept/OperatorEx03_divide_and_rest.java
@@ -0,0 +1,11 @@
+public class OperatorEx03_divide_and_rest {
+	public static void main(String[] args) {
+		int a = 11;
+		int b = 4;
+		
+		System.out.print("a / b = ");
+		System.out.println(a / b);
+		System.out.print("a % b = ");
+		System.out.println(a % b);
+	}
+}
